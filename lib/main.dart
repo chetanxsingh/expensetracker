92b5a91e87b24b6ef08c0d76c3ecd5f4ba43a7ae
chetansinghrajput <chetanrajput076@gmail.com>
2024-03-07 12:08:35 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 import 'pages/home_page.dart';
+
 void main() async{ 
 //intialize hive
   await Hive.initFlutter();
@@ -12,6 +13,10 @@ void main() async{
 
  runApp(const MyApp());
 }
+
+
+
+
 
 class MyApp extends StatelessWidget{
   const MyApp({super.key});
